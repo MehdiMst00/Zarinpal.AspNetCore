82b2b8b1bf6d86c990b6ffa1f9da59cb094b560c
MehdiMst00 <mohammadmehdi.mst@gmail.com>
2022-05-04 18:18:37 +0430

    Add MSTest For RequestPayment

---
+++ tests/Zarinpal.AspNetCore.Test/ZarinpalServiceTest.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading.Tasks;
+using Zarinpal.AspNetCore.DTOs;
+using Zarinpal.AspNetCore.Extensions;
+using Zarinpal.AspNetCore.Interfaces;
+using Zarinpal.AspNetCore.Models;
+
+namespace Zarinpal.AspNetCore.Test;
+
+[TestClass]
+public class ZarinpalServiceTest
+{
+    private readonly IZarinpalService _zarinpalService;
+
+    public ZarinpalServiceTest()
+    {
+        var serviceProvider = new ServiceCollection()
+            .AddZarinpal(options =>
+            {
+                options.ZarinpalMode = ZarinpalMode.Original;
+                options.MerchantId = "xxxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx";
+            })
+            .BuildServiceProvider();
+
+        _zarinpalService = serviceProvider.GetRequiredService<IZarinpalService>();
+    }
+
+    [TestMethod]
+    public async Task RequestPaymentTest()
+    {
+        // Arrange
+        var request = new ZarinpalRequestDTO(5000, "خرید",
+            "https://localhost:5000/Home/VerifyPayment");
+
+        // Act
+        var actual = await _zarinpalService.RequestAsync(request);
+
+        // Assert
+        Assert.IsTrue(actual.IsSuccessStatusCode);
+    }
+}
